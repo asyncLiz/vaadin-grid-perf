78b36cec3ff7600ceb9c6e82f2c0a16c8744b2a8
Liz <asyncliz@gmail.com>
2018-11-30 10:18:57 -0600

    Add patch to fix _updateItem

--- test.js
+++ test.js
@@ -90,8 +90,10 @@ class TestApp extends LitElement {
       <button @click="${this.load5}">Load 5 Items</button>
       <button @click="${this.load25}">Load 25 Items</button>
       <button @click="${this.load500}">Load 500 Items</button>
+      <button @click="${this.patch}">Patch _updateItem</button>
+      <button @click="${this.restore}">Restore _updateItem</button>
       <vaadin-grid .items="${this.items}">
-        <vaadin-grid-column .renderer="${this.renderFirstCell}"></vaadin-grid-column>
+        <vaadin-grid-column path="first" .renderer="${this.renderFirstCell}"></vaadin-grid-column>
         <vaadin-grid-column path="second"></vaadin-grid-column>
         <vaadin-grid-column path="third"></vaadin-grid-column>
         <vaadin-grid-column path="fourth"></vaadin-grid-column>
@@ -108,6 +110,40 @@ class TestApp extends LitElement {
     }
 
     render(`${model.item.first}`, root);
+  }
+
+  patch() {
+    const grid = this.shadowRoot.querySelector('vaadin-grid');
+    if (!this.originalUpdateItem) {
+      this.originalUpdateItem = grid._updateItem;
+    }
+
+    grid._updateItem = (row, item) => {
+      row._item = item;
+      const model = grid.__getRowModel(row);
+      if (
+        row._model && 
+        row._model.index === model.index && 
+        row._model.item === model.item && 
+        row._model.level === model.level && 
+        row._model.expanded === model.expanded && 
+        row._model.selected === model.selected && 
+        row._model.detailsOpened === model.detailsOpened
+      ) {
+        // The model has not changed for this row, don't re-render it
+        return;
+      }
+
+      row._model = model;
+      this.originalUpdateItem.call(grid, row, item);
+    };
+  }
+
+  restore() {
+    if (this.originalUpdateItem) {
+      const grid = this.shadowRoot.querySelector('vaadin-grid');
+      grid._updateItem = this.originalUpdateItem;
+    }
   }
 }
 
